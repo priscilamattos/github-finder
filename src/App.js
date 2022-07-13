136a428893c6716af34396a8ae91f90d8d46221c
priscilamattos <priiica12@gmail.com>
2022-07-12 23:28:39 -0400

    Config updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
 export default function App() {
   return (
-    <h1 className="bg-purple-500 text-3xl font-bold underline">Hello world!</h1>
+    <Router>
+      <div className="flex h-screen flex-col justify-between">
+        <Navbar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }

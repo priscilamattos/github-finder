365fe082dd4c70431fde3866ffddb289c962395e
priscilamattos <priiica12@gmail.com>
2022-07-13 17:06:23 -0400

    New components and fetch data from Github API.

---
+++ src/components/users/UserItem.jsx
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const UserItem = ({ user: { login, avatar_url } }) => {
+  return (
+    <div className="side card compact bg-base-100 shadow-md">
+      <div className="card-body flex-row items-center space-x-4">
+        <div>
+          <div className="avatar">
+            <div className="h-14 w-14 rounded-full shadow">
+              <img src={avatar_url} alt="Profile" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className="card-title">{login}</h2>
+          <Link
+            className="text-base-content text-opacity-40"
+            to={`/user/${login}`}
+          >
+            Visit Profile
+          </Link>
+        </div>
+      </div>
+      {login}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+export default UserItem;
